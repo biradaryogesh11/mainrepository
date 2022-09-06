6ed72a44b0076904b8bf21fda757a99f903c014b
biradaryogesh11 <112967452+biradaryogesh11@users.noreply.github.com>
2022-09-06 21:55:30 +0530

    new method is added

---
+++ Github/src/com/qa/pageLayer/A.java
@@ -0,0 +1,9 @@
+package com.qa.pageLayer;
+
+public class A 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Test method ");
+    }
+}
---
+++ Github/src/com/qa/pageLayer/B.java
@@ -0,0 +1,9 @@
+package com.qa.pageLayer;
+
+public class B 
+{
+   public static void main (String args[])
+   {
+	  System.out.println("test method B"); 
+   }
+}
